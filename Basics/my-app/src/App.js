40c00e7f3ff47824113d8efe1c1af84fb0aedfe4
livealvi <livealvi@gmail.com>
2021-06-23 12:08:25 +0600

    first commit

---
+++ Basics/my-app/src/App.js
@@ -0,0 +1,16 @@
+//import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+      <h1>Lets Start!</h1>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
