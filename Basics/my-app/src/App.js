f66daccf0b8f0a6f8cd7a437012dade319ec97d4
livealvi <livealvi@gmail.com>
2021-06-29 15:00:52 +0600

    CSS & Date - Add

--- Basics/my-app/src/App.js
+++ Basics/my-app/src/App.js
@@ -1,14 +1,39 @@
-//import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+//import "./App.css";
+//import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses.js";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.67,
+      date: new Date(2021, 3, 28),
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 794.67,
+      date: new Date(2021, 3, 29),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 3, 29),
+    },
+    {
+      id: "e4",
+      title: "Mew Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 3, 30),
+    },
+  ];
+
   return (
     <div>
       <h1>Lets Start!</h1>
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
 }
--- Basics/my-app/src/components/ExpenseItem.js
+++ Basics/my-app/src/components/ExpenseItem.js
@@ -1,20 +1,17 @@
-import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+import "./ExpenseItem.css";
 
-function ExpenseItem() {
-
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.65;
-
-    return (
-        <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
-            <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">{expenseAmount}</div>
-            </div>
-        </div>
-    );
+function ExpenseItem(props) {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
+    </Card>
+  );
 }
 
 export default ExpenseItem;
