01db74e54343ccdbc530d575c6268c7a730e00f1
Alvi Hasan <livealvi@gmail.com>
2021-11-27 22:54:35 +0600

    Functional Components

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,23 +1,14 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
+import { Greet } from "./components/Greet";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Greet />
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ hello-world/src/components/Greet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// function Greet() {
+//   return <h1>Hello, Alvi</h1>;
+// }
+
+export const Greet = () => {
+  return <h1>Hello, Alvi.</h1>;
+};
+
+//export default Greet;
