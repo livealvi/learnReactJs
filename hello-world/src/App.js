3ab8975bf1fa7cd233d9a5c61b0a03d829a08ed4
Alvi Hasan <livealvi@gmail.com>
2021-11-28 11:41:32 +0600

    Class Components

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Greet } from "./components/Greet";
+import Welcome from "./components/Welcome";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Greet />
+        <Welcome />
       </div>
     );
   }
