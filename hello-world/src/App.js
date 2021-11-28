fe15588a21dabc416eb5f11ba69b745e975f854a
Alvi Hasan <livealvi@gmail.com>
2021-11-28 18:28:30 +0600

    Methods as props

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -8,12 +8,14 @@ import Counter from "./components/Counter";
 import FunctionClick from "./components/FunctionClick";
 import ClassClick from "./components/ClassClick";
 import EventBind from "./components/EventBind";
+import ParentComponent from "./components/ParentComponent";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <EventBind />
+        <ParentComponent />
+        {/* <EventBind /> */}
         {/* <FunctionClick />
         <ClassClick /> */}
         {/* <Message />
